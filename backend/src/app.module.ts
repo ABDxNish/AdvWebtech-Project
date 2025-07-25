5025dca834484998ef62eeb3bb1617ae44059dac
Khondaker Abdullah Nishat <110023608+ABDxNish@users.noreply.github.com>
2025-07-25 19:27:05 +0600

    File validation added and entity created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminController } from './Admin/admin.controller';
 import { AdminModule } from './Admin/admin.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule,TypeOrmModule.forRoot(
+ { type: 'postgres',
+ host: 'localhost',
+ port: 5432,
+ username: 'postgres',
+ password: 'KHNISHAT172',
+ database: 'AdminProject',// database name
+ autoLoadEntities: true,
+ synchronize: true,
+ } ),
+],
   controllers: [AppController],
   providers: [AppService],
 })
