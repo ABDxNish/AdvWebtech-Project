ef6c0722c3be4beffd09a43ef112e9572e2a843d
shafinsan <eliasjabershafin599@gmail.com>
2025-08-17 14:58:45 +0600

    mid term project

---
+++ src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleModule } from './role/role.module';
+import { AuthModule } from './auth/auth.module';
+
+import { ConfigModule } from '@nestjs/config';
+import { CustomerServiceModule } from './customer-service/customer-service.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { AdminUserControlModule } from './admin-user-control/admin-user-control.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'system',
+      database: 'TravelDatabase',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    RoleModule,
+    AuthModule,
+    AdminUserControlModule,
+
+    CustomerServiceModule,
+
+    AdminUserControlModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
